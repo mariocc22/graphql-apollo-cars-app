907e88ef54be0c5880dc6e48b386ffceb3f4da13
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-10-14 00:41:39 -0700

    feat: Adding Apollo Server

---
+++ server/src/peopleCarsScheme.js
@@ -0,0 +1,133 @@
+//  1. define the schema
+const people = [
+  {
+    id: "1",
+    firstName: "Bill",
+    lastName: "Gates",
+  },
+  {
+    id: "2",
+    firstName: "Steve",
+    lastName: "Jobs",
+  },
+  {
+    id: "3",
+    firstName: "Linux",
+    lastName: "Torvalds",
+  },
+];
+
+// 2. define the type definitions for the schema
+const typeDefs = `
+  type People {
+    id: String!
+    firstName: String
+    lastName: String
+  }
+
+  type Query {
+    people: [People]
+    person(id: String!): People
+  }
+
+  type Mutation {
+    addPerson(id: String!, firstName: String!, lastName: String!): People
+  }
+`;
+
+// 3. define the resolvers for the schema fields (Query) defined in step 2 above (typeDefs) and the data defined in step 1 above (people)
+const resolvers = {
+  Query: {
+    people: () => people,
+    person: (root, { id }) => people.find((person) => person.id === id),
+  },
+  Mutation: {
+    addPerson: (root, args) => {
+      const newPerson = {
+        id: args.id,
+        firstName: args.firstName,
+        lastName: args.lastName,
+      };
+      people.push(newPerson);
+      return newPerson;
+    },
+  },
+};
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+const cars = [
+  {
+    id: "1",
+    year: "2019",
+    make: "Toyota",
+    model: "Corolla",
+    price: "40000",
+    personId: "1",
+  },
+  {
+    id: "2",
+    year: "2018",
+    make: "Lexus",
+    model: "LX 600",
+    price: "13000",
+    personId: "1",
+  },
+  {
+    id: "3",
+    year: "2017",
+    make: "Honda",
+    model: "Civic",
+    price: "20000",
+    personId: "1",
+  },
+  {
+    id: "4",
+    year: "2019",
+    make: "Acura ",
+    model: "MDX",
+    price: "60000",
+    personId: "2",
+  },
+  {
+    id: "5",
+    year: "2018",
+    make: "Ford",
+    model: "Focus",
+    price: "35000",
+    personId: "2",
+  },
+  {
+    id: "6",
+    year: "2017",
+    make: "Honda",
+    model: "Pilot",
+    price: "45000",
+    personId: "2",
+  },
+  {
+    id: "7",
+    year: "2019",
+    make: "Volkswagen",
+    model: "Golf",
+    price: "40000",
+    personId: "3",
+  },
+  {
+    id: "8",
+    year: "2018",
+    make: "Kia",
+    model: "Sorento",
+    price: "45000",
+    personId: "3",
+  },
+  {
+    id: "9",
+    year: "2017",
+    make: "Volvo",
+    model: "XC40",
+    price: "55000",
+    personId: "3",
+  },
+];
+
+export { typeDefs, resolvers };
